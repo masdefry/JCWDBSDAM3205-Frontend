cf8ab1d2d4c96bf1a413f4eb798a6637d8a36cba
masdefry <muhammaddefryan@gmail.com>
2025-09-24 09:14:50 +0700

    init new next-app

---
+++ furniro-app/src/app/page.tsx
@@ -0,0 +1,42 @@
+import Image from 'next/image';
+import Navbar from '@/components/navbar';
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <div className='flex'>
+        <div
+          style={{
+            position: 'relative',
+            width: '100px',
+            height: '300px',
+            backgroundColor: 'red',
+          }}
+        >
+          <Image
+            src={'/kucing.jpg'}
+            alt='Image'
+            fill
+            style={{ objectFit: 'contain' }}
+          />
+        </div>
+        <div
+          style={{
+            position: 'relative',
+            width: '100px',
+            height: '300px',
+            backgroundColor: 'red',
+          }}
+        >
+          <Image
+            src={'/kucing.jpg'}
+            alt='Image'
+            fill
+            style={{ objectFit: 'contain' }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ intro-next/src/app/data-fetching/isr/page.tsx
@@ -0,0 +1,16 @@
+export default async function Page() {
+  const res = await fetch(`http://localhost:3001/products`, {
+    next: { revalidate: 10 },
+  });
+  const products = await res.json();
+  return (
+    <div>
+      <h1>SSG - Static Site Generator</h1>
+      <ol>
+        {products?.map((product: any, index: any) => {
+          return <li key={index}>{product?.name}</li>;
+        })}
+      </ol>
+    </div>
+  );
+}
